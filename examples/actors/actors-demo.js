1d32593837d0631c205c16b44a7131ef0d250875
RidgeBatty <54147586+RidgeBatty@users.noreply.github.com>
2021-12-04 00:12:02 +0200

    Improved Actors demo, added comments

--- examples/actors/actors-demo.js
+++ examples/actors/actors-demo.js
@@ -1,3 +1,14 @@
+/*
+
+    In this demo two Actors are created from images. Some basic functions such as Actor rotation, scaling and movement are demonstrated.
+    The first Actor is a space themed background and the other one is a spaceship.
+    By default, all new Actors are placed on the same z-layer. Because the background in this demo is created first, it also gets drawn first
+    The dashed red box represents a viewport; 
+    The renderingSurface (the larger gray rectangle) is not cleared between draw calls. Therefore the rotating spaceship images remain on the screen if the background does not cover them.
+    Typically it is not necessary to clear the renderingSurface between frames, if the background already fills the viewport.
+
+*/
+
 import * as TGE from '../../engine.js';
 import { Vector } from '../../types.js';
 const Engine = TGE.Engine;	
@@ -19,7 +30,7 @@ const main = async () => {
 
     let ship;
     try {        
-        await Engine.addActor('actor', { imgUrl:'img/level1.jpg', scale:0.5, position:new Vector(540, 330) });                
+        await Engine.addActor('actor', { imgUrl:'img/level1.jpg', scale:0.5, position:new Vector(556, 330) });                
 
         ship = await Engine.addActor('actor', { imgUrl:'img/spaceship.png' });
 
