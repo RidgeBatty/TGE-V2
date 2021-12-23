f6c283e23c49d3166278f63a9b17dc0cf0fdc84a
RidgeBatty <54147586+RidgeBatty@users.noreply.github.com>
2021-12-23 23:59:30 +0200

    Enemy.js

---
+++ enemy.js
@@ -0,0 +1,23 @@
+/*
+
+	Enemy
+
+*/
+
+import { Actor } from './actor.js';
+import { Engine } from './engine.js';
+import { Vector2 } from './types.js';
+import { Hitpoints } from './actor-hp.js';
+
+class Enemy extends Actor {
+	constructor(o) {								
+		const params = Object.assign(o, { defaultColliderType : 'Enemy' });
+		super(params);
+				
+		this.instigator = ('instigator' in o) ? o.instigator : null;
+	}
+	
+	get isEnemy() { return true; }
+}
+
+export { Enemy }
