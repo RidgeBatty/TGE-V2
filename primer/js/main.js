19de3484a80d8a2f81a7a073a36609f3177f8d94
RidgeBatty <54147586+RidgeBatty@users.noreply.github.com>
2023-07-23 15:19:23 +0300

    Updated primer

--- primer/js/main.js
+++ primer/js/main.js
@@ -1,19 +1,17 @@
 import * as TGE from './v2/engine.js';
-const Engine = TGE.Engine;	
+const { Engine } = TGE;
 
-// Your game's main execution point:
-const main = async () => {    
-/* 
- * ID of the HTML element where the game engine should draw
- * hasEdges:Actors should respect the viewport edges.
- * hasRenderingSurface: Engine should create a Canvas element for rendering
- */
-    Engine.setup({ rootElem:'game', flags:{ hasEdges:false, hasRenderingSurface:true } });    
-    
-    // do your game stuff here...
-
-    Engine.start(tick);             // start the engine (with optional reference to your custom callback function, executed on every gameLoop tick)
+const tick = () => {
+    console.log('This code will run every tick 60 frames per second by default.')
+    console.log('Be careful with performance, do not add computationally heavy operations here!');
 }
 
-// Calls your custom initialization function to start the game when init is completed
-Engine.init(main);    
+const main = async () => {                      // main() will be your game's execution point
+    await Engine.setup('./settings.hjson');     // Load settings from external file (this is the preferred way to set up the engine)
+    
+    // do your game setup/initialization stuff here...
+
+    Engine.start(tick);                         // start the engine (with optional reference to your custom callback function, executed on every gameLoop tick)
+}
+
+Engine.init(main);                              // Calls your custom initialization function to start the game when init is completed
